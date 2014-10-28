971be1057a48c82eff6bc4f64caa3dfb3b8d2742
XhmikosR <xhmikosr@gmail.com>
2014-10-28 23:50:22 +0200

    WIP

--- docs/assets/js/src/application.js
+++ docs/assets/js/src/application.js
@@ -174,3 +174,39 @@
   })
 
 }(jQuery)
+
+// Code taken from Jekyll docs
+;(function () {
+  'use strict';
+
+  var anchorForId = function (id) {
+    var anchor = document.createElement('a');
+    anchor.className = 'anchor';
+    anchor.href      = '#' + id;
+    anchor.innerHTML = '<span class="glyphicon glyphicon-link"></span>';
+    return anchor;
+  };
+
+  var linkifyAnchors = function (level, containingElement) {
+    var headers = containingElement.getElementsByTagName('h' + level);
+    for (var h = 0; h < headers.length; h++) {
+      var header = headers[h];
+
+      if (typeof header.id !== 'undefined' && header.id !== '') {
+        header.appendChild(anchorForId(header.id));
+      }
+    }
+  };
+
+  document.onreadystatechange = function () {
+    if (this.readyState === 'complete') {
+      var contentBlock = document.getElementsByClassName('bs-docs-container')[0];
+      if (!contentBlock) {
+        return;
+      }
+      for (var level = 1; level <= 6; level++) {
+        linkifyAnchors(level, contentBlock);
+      }
+    }
+  };
+})();
